ac124d4c3ae19b2e9d233ce9e743e19f9aaabaa2
meeeeeeeee <me@you.org>
2025-05-26 10:32:39 +0000

    added git signed commits

---
+++ install-node-exporter.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+wget https://github.com/prometheus/node_exporter/releases/download/v1.9.1/node_exporter-1.9.1.linux-amd64.tar.gz
+tar xvfz node_exporter-1.9.1.linux-amd64.tar.gz
+#cd node_exporter-1.9.1.linux-amd64
+#./node_exporter
+/etc/systemd/system/
+useradd node_exporter
+cat > node_exporter.service <<- "EOF"
+#Node Exporter service file — /etc/systemd/system/node_exporter.service
+[Unit]
+Description=Node Exporter
+Wants=network-online.target
+After=network-online.target
+
+[Service]
+User=node_exporter
+Group=node_exporter
+Type=simple
+ExecStart=/usr/local/bin/node_exporter
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo systemctl daemon-reload
+sudo systemctl start node_exporter
+sudo systemctl enable node_exporter
