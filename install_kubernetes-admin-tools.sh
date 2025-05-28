4f03f9d503b25ebe485cc40a2fa1c87cee152055
meeeeeeeee <me@you.org>
2025-05-28 16:03:30 +0000

    guff

--- install_kubernetes-admin-tools.sh
+++ install_kubernetes-admin-tools.sh
@@ -16,3 +16,11 @@ curl -sL "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_ch
 tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
 
 sudo mv /tmp/eksctl /usr/local/bin
+
+#Install Helm
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+
+#Install K9s
+curl -sS https://webinstall.dev/k9s | bash
